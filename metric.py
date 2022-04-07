d82a64a6a3d6419c50bed50808f1aa80f7b22699
HyeokJong <jj770206@naver.com>
2022-04-08 03:47:08 +0900

    mean

--- metric.py
+++ metric.py
@@ -3,4 +3,4 @@ import torch
 def metric_function(output, target):
     _, argmax = torch.max(output, dim = 1)
     corrects = (argmax == target).sum()
-    return corrects.item()    # torch로 나와서 item만 뽑습니다.
+    return corrects.item()/target.shape[0]    # torch로 나와서 item만 뽑습니다.
